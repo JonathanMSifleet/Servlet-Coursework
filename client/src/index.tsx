98bf328ddc22cade4e36590a4bd0766eb49d6083
Jonathan Sifleet <41328055+JonathanMSifleet@users.noreply.github.com>
2022-01-08 00:32:34 +0000

    Misc

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,11 +2,11 @@
 import '@fontsource/roboto';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import { StrictMode } from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import Home from './pages/Home/Home';
 import './styles/global.scss';
 
-ReactDOM.render(
+render(
   <StrictMode>
     <Home />
   </StrictMode>,
