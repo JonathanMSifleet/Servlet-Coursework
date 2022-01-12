19c1899a69f9e6a9afb9ddf18a9bec3e13e531f3
Jonathan Sifleet <41328055+JonathanMSifleet@users.noreply.github.com>
2022-01-12 15:14:53 +0000

    Update xmlToJSON.ts

--- client/src/utils/xmlToJSON.ts
+++ client/src/utils/xmlToJSON.ts
@@ -4,12 +4,12 @@ export const monoXMLFilmToJSON = (xmlToParse: string): IFilm => {
   const parsedXML = new DOMParser().parseFromString(xmlToParse, 'application/xml');
   const xmlDocument = parsedXML.getElementsByTagName('film')[0].children;
 
-  const id = parseInt(xmlDocument[0].innerHTML);
-  const title = xmlDocument[1].innerHTML;
-  const year = parseInt(xmlDocument[2].innerHTML);
-  const director = xmlDocument[3].innerHTML;
-  const stars = xmlDocument[4].innerHTML;
-  const review = xmlDocument[5].innerHTML;
+  const id = parseInt(xmlDocument[0].textContent!);
+  const title = xmlDocument[1].textContent!;
+  const year = parseInt(xmlDocument[2].textContent!);
+  const director = xmlDocument[3].textContent!;
+  const stars = xmlDocument[4].textContent!;
+  const review = xmlDocument[5].textContent!;
 
   return { id, title, year, director, stars, review };
 };
